a52d1c57bddfc357f25b20f8863d0ce50ff7b3ea
Nwokenta <ezinnanwok615@gmail.com>
2024-03-14 11:02:19 -0700

    Printing Names Stable - Pre ChatGPT

--- PrintingNamesIndex.py
+++ PrintingNamesIndex.py
@@ -1,4 +1,11 @@
 # Programmer: Ezinna Nwokenta
-# Date 3.14.2024
-#program: Printing Names Index
-#resource: https://www.youtube.com/watch?v=jRAAaDll34Q
+# Date: 3.14.2024
+# program: Printing Names Index
+# resource: https://www.youtube.com/watch?v=jRAAaDll34Q
+
+names = ["Corey", "Travis", "Chris", "Dave"]
+
+index = 0
+for name in names:
+    print(index,name)
+    index += 1
